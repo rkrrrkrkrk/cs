349311e4195e5c8c7f6e60d6de67a8c3423b61c6
rkrrrkrkrk <106737875+rkrrrkrkrk@users.noreply.github.com>
2024-01-23 17:10:13 +0900

    cs.0

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+ 
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nArr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+ 
+            Console.WriteLine("nArr[{0}] = {1}", 0, nArr[0]);
+            Console.WriteLine("nArr[{0}] = {1}", 3, nArr[3]);
+            Console.WriteLine();
+ 
+ 
+            int nIndex = 0;
+ 
+            foreach(int i in nArr)
+            {
+                Console.WriteLine("nArr[{0}] = {1}", nIndex++, i);
+            }
+        }
+    }
+}
